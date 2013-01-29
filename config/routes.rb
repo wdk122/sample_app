a2bbfbf1daaf8620b8fff8ae139697e3da602f20
Bill Kilmer <wdk122@gmail.com>
2013-01-28 20:49:17 -0800

    Finish layout and routes, finish Chapter 5

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,13 @@
 SampleApp::Application.routes.draw do
-  get "static_pages/home"
+  get "users/new" #this line doesn't follow REST
 
-  get "static_pages/help"
+  root to: 'static_pages#home'
 
-  get "static_pages/about"
+  match '/signup',   to: 'users#new'
+  
+  match "/help",     to: 'static_pages#help' #matches "/help" to the help action in StaticPages controller
+  match "/about",    to: 'static_pages#about'
+  match "/contact",  to: 'static_pages#contact' 
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
