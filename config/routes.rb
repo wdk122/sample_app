ef6136548c40234f2be81fa96ea49b294b1d4518
Bill Kilmer <wdk122@gmail.com>
2013-01-30 18:17:47 -0800

    Finish user signup-page 318

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 SampleApp::Application.routes.draw do
-  get "users/new" #this line doesn't follow REST
+  
+  resources :users
 
   root to: 'static_pages#home'
 
@@ -7,7 +8,8 @@ SampleApp::Application.routes.draw do
   
   match "/help",     to: 'static_pages#help' #matches "/help" to the help action in StaticPages controller
   match "/about",    to: 'static_pages#about'
-  match "/contact",  to: 'static_pages#contact' 
+  match "/contact",  to: 'static_pages#contact'
+
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
