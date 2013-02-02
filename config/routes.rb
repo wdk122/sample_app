5963b7ad10337f2599629832f61a457c74451ede
Bill Kilmer <wdk122@gmail.com>
2013-02-01 17:01:16 -0800

    finish sign in chapter 8 pg 372

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,15 @@
 SampleApp::Application.routes.draw do
   
   resources :users
+  resources :sessions, only: [:new, :create, :destroy]
+
+
 
   root to: 'static_pages#home'
 
   match '/signup',   to: 'users#new'
+  match '/signin',   to: 'sessions#new'
+  match '/signout',  to: 'sessions#destroy', via: :delete
   
   match "/help",     to: 'static_pages#help' #matches "/help" to the help action in StaticPages controller
   match "/about",    to: 'static_pages#about'
